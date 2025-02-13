a94a815cdf8dc9f085dcb77d6c044da0d3311c71
thea <theanasr29@gmail.com>
2025-02-13 15:53:36 +0200

    .

--- src/exam2/Main.java
+++ src/exam2/Main.java
@@ -5,10 +5,10 @@ public class Main {
         Bookstore bookstore=new Bookstore();
         bookstore.addBook("Persepolis", "M.S", 20, 5);
         bookstore.addBook("Percy Jackson", "R.G", 12, 5);
-        Customer customer=new Customer("Maria Sfeir", "maria@gmai.com", "09876543");
+        Customer customer=new Customer("Maria Sfeir", "maria@gmail.com", "09876543");
         Book book=bookstore.searchBook("Persepolis");
-        bookstore.buy(book, new PercentageDiscount(0.2));
-        bookstore.buy(book, new PercentageDiscount(0.2));
+        bookstore.buy(book, new PercentageDiscount(0.12));
+        bookstore.buy(book, new PercentageDiscount(0.4));
         bookstore.buy(book, new PercentageDiscount(0.2));
         bookstore.buy(book, new FixedDiscount(6));
         bookstore.buy(book, null);
